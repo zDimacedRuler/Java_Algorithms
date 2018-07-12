e2bba0ff14c9987a37bc85078e929ec1ed3e8b4d
zDimacedRuler <zdcyberboy@gmail.com>
2018-07-12 16:40:02 +0530

    Kadane Algo done

--- Ravindra_Babu/src/Arrays/MaxSumSubArray.java
+++ Ravindra_Babu/src/Arrays/MaxSumSubArray.java
@@ -1,4 +1,5 @@
 package Arrays;
+
 import java.util.Scanner;
 
 /*
@@ -15,24 +16,74 @@ public class MaxSumSubArray {
             arr[i] = scan.nextInt();
         }
         //The algorithm
+        maxSubArraySum(arr);
+        kadane(arr, length);
+        maxSumRavi(arr, length);
+        scan.close();
+    }
+
+    /*
+     * Simple observation: if the index is at i, then (currentSum + array[i]) will be the cuurentSum or
+     * the current element arr[i] will only be the currentSum
+     * And when this current sum is greater than maxSu it is maxSum
+     * */
+    private static void kadane(int[] arr, int length) {
+        int maxSum = arr[0], currentSum = arr[0];
+        int a = 0, b = 0;
+        int l = 0, f = 0;
+        for (int i = 1; i < length; i++) {
+//            this simple is of two lines
+
+//            currentSum = Math.max(arr[i], currentSum + arr[i]);
+//            maxSum = Math.max(maxSum, currentSum);
+
+//            or-- below algo to print the index of the subArray
+
+            int temp = currentSum + arr[i];
+            if (temp > arr[i]) {
+                b++;
+                currentSum = temp;
+            } else {
+                a = i;
+                b = i;
+                currentSum = arr[i];
+            }
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                l = a;
+                f = b;
+            }
+        }
+        System.out.println("Kadane The max sum is:" + maxSum);
+        System.out.println("The left index is:" + l);
+        System.out.println("The right index is:" + f);
+    }
+
+    static void maxSubArraySum(int a[]) {
+        int size = a.length;
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+
+        for (int i = 0; i < size; i++) {
+            max_ending_here = max_ending_here + a[i];
+            if (max_so_far < max_ending_here)
+                max_so_far = max_ending_here;
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+        }
+        System.out.println("Geek The max sum is:" + max_so_far);
+    }
+
+    private static void maxSumRavi(int[] arr, int length) {
         int max_sum = Integer.MIN_VALUE;
-        int a, b, s;
-        a = b = s = 0;
         for (int i = 1; i < length; i++) {
             if (arr[i - 1] > 0) {
                 arr[i] += arr[i - 1];
-                s++;
-            } else {
-                s = 0;
             }
             if (max_sum < arr[i]) {
                 max_sum = arr[i];
-                a = s;
-                b = i;
             }
         }
-        System.out.println("The a:" + a + " b:" + b);
-        System.out.println("The max sum is:" + max_sum);
-        scan.close();
+//        System.out.println("The a:" + a + " b:" + b);
+        System.out.println("Babu The max sum is:" + max_sum);
     }
 }
